dd53d8c2517376add797481887492bf61f0b05d8
John Schlesinger <johnschlesinger5@gmail.com>
2022-02-04 01:28:02 +0000

    Add breadcrumbs

--- pages/folders/index.tsx
+++ pages/folders/index.tsx
@@ -1,34 +1,13 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getSession } from 'next-auth/react';
-import Layout from '../../components/layout';
 import FoldersList from '../../components/folders/FoldersList';
 import LoadingIndicator from '../../components/ui/LoadingIndicator';
-import { wrapper } from '../../store';
-import { setUser } from '../../store/auth/reducer';
 import { fetchFoldersInit } from '../../store/folders/reducer';
 import { selectFolders } from '../../store/folders/selectors';
 import { selectIsLoading } from '../../store/loading/selectors';
+import { serverSideAuthentication } from '../../lib/auth';
 
-export const getServerSideProps = wrapper.getServerSideProps((store) => async (ctx): Promise<any> => {
-    const session = await getSession({ req: ctx.req });
-
-    if (!session) {
-        store.dispatch(setUser(null));
-        return {
-            redirect: {
-                destination: '/signin',
-                permanent: false,
-            },
-        };
-    }
-
-    store.dispatch(setUser(session.user));
-
-    return {
-        props: { session },
-    };
-});
+export const getServerSideProps = serverSideAuthentication();
 
 const FoldersPage = () => {
     const dispatch = useDispatch();
@@ -39,11 +18,7 @@ const FoldersPage = () => {
         dispatch(fetchFoldersInit());
     }, []);
 
-    return (
-        <>
-            {isLoading ? <LoadingIndicator /> : <FoldersList folders={folders} />}
-        </>
-    );
+    return <>{isLoading ? <LoadingIndicator /> : <FoldersList folders={folders} />}</>;
 };
 
 export default FoldersPage;
