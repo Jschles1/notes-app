8394700f863f7b4bc84638975d5cb5d67276bb10
John Schlesinger <johnschlesinger5@gmail.com>
2022-02-20 21:10:49 +0000

    Add timestamps to models

--- lib/server/models/Folder.ts
+++ lib/server/models/Folder.ts
@@ -2,17 +2,22 @@ import mongoose, { Schema } from 'mongoose';
 
 const { String, ObjectId } = Schema.Types;
 
-export const folderSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
+export const folderSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        notes: [{ type: ObjectId, ref: 'notes' }],
+        user: {
+            type: ObjectId,
+            ref: 'users',
+        },
     },
-    notes: [{ type: ObjectId, ref: 'notes' }],
-    user: {
-        type: ObjectId,
-        ref: 'users',
-    },
-});
+    {
+        timestamps: true,
+    }
+);
 
 const Folder = mongoose.models.folders || mongoose.model('folders', folderSchema);
 
