7c957b793116e5ae79d3144f399faca2141a0ffa
John Schlesinger <johnschlesinger5@gmail.com>
2022-03-03 01:03:52 +0000

    Add NoteDetail and update note functionality

--- store/rootSaga.ts
+++ store/rootSaga.ts
@@ -1,8 +1,8 @@
 import { all, takeEvery } from 'redux-saga/effects';
 import { createFolderInit, deleteFolderInit, fetchFoldersInit, updateFolderInit } from './folders/reducer';
-import { fetchNotesInit, createNoteInit } from './notes/reducer';
+import { fetchNotesInit, createNoteInit, updateNoteInit } from './notes/reducer';
 import { createFolderSaga, deleteFolderSaga, fetchFoldersSaga, updateFolderSaga } from './folders/saga';
-import { fetchNotesSaga, createNoteSaga } from './notes/saga';
+import { fetchNotesSaga, createNoteSaga, updateNoteSaga } from './notes/saga';
 
 function* watchFolders() {
     yield takeEvery(fetchFoldersInit.type, fetchFoldersSaga);
@@ -14,6 +14,7 @@ function* watchFolders() {
 function* watchNotes() {
     yield takeEvery(fetchNotesInit.type, fetchNotesSaga);
     yield takeEvery(createNoteInit.type, createNoteSaga);
+    yield takeEvery(updateNoteInit.type, updateNoteSaga);
 }
 
 export default function* rootSaga() {
