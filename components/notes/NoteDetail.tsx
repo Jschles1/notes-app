1f812a53ece94014c9f2776c1a7d8f12a1841fa4
John Schlesinger <johnschlesinger5@gmail.com>
2022-03-03 01:05:11 +0000

    Delete note UI

--- components/notes/NoteDetail.tsx
+++ components/notes/NoteDetail.tsx
@@ -1,6 +1,18 @@
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Box, Button, styled, TextField, TextFieldProps, Typography } from '@mui/material';
+import {
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    styled,
+    TextField,
+    TextFieldProps,
+    Typography,
+} from '@mui/material';
 import Card from '../ui/Card';
 import { selectIsLoading } from '../../store/loading/selectors';
 import { Note } from '../../interfaces';
@@ -34,11 +46,18 @@ interface Props {
 const NoteDetail: React.FC<Props> = ({ note, folderId, noteId }) => {
     const dispatch = useDispatch();
     const [isUpdating, setIsUpdating] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
     const isLoading = useSelector(selectIsLoading);
 
     const onUpdate = (data) => {
         const payload = { ...data, folderId, noteId };
         dispatch(updateNoteInit(payload));
+    };
+
+    const onDelete = () => {};
+
+    const handleClose = () => {
+        setOpen(false);
     };
 
     React.useEffect(() => {
@@ -54,61 +73,85 @@ const NoteDetail: React.FC<Props> = ({ note, folderId, noteId }) => {
             isUpdating
         />
     ) : (
-        <Card
-            sx={{
-                height: '100%',
-                display: 'flex',
-                flexDirection: 'column',
-            }}
-        >
-            <Box
+        <>
+            <Card
                 sx={{
+                    height: '100%',
                     display: 'flex',
-                    alignItems: 'start',
-                    justifyContent: 'space-between',
+                    flexDirection: 'column',
                 }}
             >
-                <Box>
-                    <Typography
-                        variant="h4"
-                        sx={{
-                            marginBottom: 1,
-                            marginTop: 0,
-                            lineHeight: 1,
-                        }}
-                    >
-                        <Skeleton>{note?.name}</Skeleton>
-                    </Typography>
-                    <Typography paragraph>
-                        <Skeleton>Last updated: {formatDate(note?.updatedAt)}</Skeleton>
-                    </Typography>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'start',
+                        justifyContent: 'space-between',
+                    }}
+                >
+                    <Box>
+                        <Typography
+                            variant="h4"
+                            sx={{
+                                marginBottom: 1,
+                                marginTop: 0,
+                                lineHeight: 1,
+                            }}
+                        >
+                            <Skeleton>{note?.name}</Skeleton>
+                        </Typography>
+                        <Typography paragraph>
+                            <Skeleton>Last updated: {formatDate(note?.updatedAt)}</Skeleton>
+                        </Typography>
+                    </Box>
+
+                    <Box>
+                        <Button
+                            variant="outlined"
+                            sx={{ marginRight: 2, textTransform: 'unset' }}
+                            onClick={() => setIsUpdating(true)}
+                            startIcon={<Edit />}
+                            disabled={isLoading}
+                        >
+                            Update Note
+                        </Button>
+                        <Button
+                            variant="outlined"
+                            sx={{ textTransform: 'unset' }}
+                            color="error"
+                            onClick={() => setOpen(true)}
+                            startIcon={<Delete />}
+                            disabled={isLoading}
+                        >
+                            Delete Note
+                        </Button>
+                    </Box>
                 </Box>
 
-                <Box>
-                    <Button
-                        variant="outlined"
-                        sx={{ marginRight: 2, textTransform: 'unset' }}
-                        onClick={() => setIsUpdating(true)}
-                        startIcon={<Edit />}
-                        disabled={isLoading}
-                    >
-                        Update Note
-                    </Button>
-                    <Button
-                        variant="outlined"
-                        sx={{ textTransform: 'unset' }}
-                        color="error"
-                        onClick={() => {}}
-                        startIcon={<Delete />}
-                        disabled={isLoading}
-                    >
-                        Delete Note
-                    </Button>
-                </Box>
-            </Box>
+                <NoteDescription value={note?.description} />
+            </Card>
 
-            <NoteDescription value={note?.description} />
-        </Card>
+            <Dialog
+                open={open}
+                onClose={() => setOpen(false)}
+                aria-labelledby="delete-confirm-title"
+                aria-describedby="delete-confirm-description"
+            >
+                <DialogTitle id="delete-confirm-title">{'Delete Note?'}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="delete-confirm-description">
+                        Are you sure you want to delete "{note?.name}"?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="outlined" color="success" onClick={onDelete} disabled={isLoading}>
+                        Confirm
+                    </Button>
+                    <Button variant="outlined" color="error" onClick={handleClose} autoFocus disabled={isLoading}>
+                        Cancel
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 };
 
