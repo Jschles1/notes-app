5e2e63d235efa15e7c4ce33254c533fd3ae0f4d0
John Schlesinger <johnschlesinger5@gmail.com>
2022-07-31 19:55:41 +0000

    Mock useLoggedInUser

--- jest.setup.tsx
+++ jest.setup.tsx
@@ -14,11 +14,13 @@ import store from '@store/index';
 import theme from '@lib/theme';
 import { handlers } from '@lib/graphql/mocks/handlers';
 
+const testUser = { username: 'admin', email: 'admin@email.com' };
+
 jest.mock('next-auth/react', () => {
     const originalModule = jest.requireActual('next-auth/react');
     const mockSession = {
         expires: new Date(Date.now() + 2 * 86400).toISOString(),
-        user: { username: 'admin', email: 'admin@email.com' },
+        user: testUser,
     };
     return {
         __esModule: true,
@@ -68,6 +70,15 @@ jest.mock('@lib/graphql/hooks', () => ({
             revalidate: jest.fn(() => {}),
             error: null,
         };
+    }),
+}));
+
+jest.mock('@lib/hooks/useLoggedInUser', () => ({
+    __esModule: true,
+    default: () => ({
+        user: testUser,
+        email: testUser.email,
+        isLoading: false,
     }),
 }));
 
